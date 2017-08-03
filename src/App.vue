aafae949fcfbeafae59f9e245294598e010722ec
hanhun <772474021@qq.com>
2017-08-03 17:12:50 +0800

    firtst try

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <!--vue-router1.0版本的路由写法-->
+      <div class="tab-item">
+        <a v-link="{path:'/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/seller'}">商家</a>
+      </div>
+    </div>
+    <router-view :seller="seller" keep-alive></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import { urlParse } from './common/js/util';
+  import header from './components/header/header';
+
+  const ERR_OK = 0;
+
+  export default {
+    data () {
+      return {
+        seller: {
+          id: (() => {
+            let queryParam = urlParse();
+            return queryParam.id;
+          })()
+        }
+      };
+    },
+    created () {
+      this.$http.get('./api/seller?id=' + this.seller.id).then((response) => {
+        response = response.body;
+//        console.log(response);
+        if (response.errno === ERR_OK) {
+          this.seller = Object.assign({}, this.seller, response.data);
+        }
+      });
+    },
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  /*引入样式*/
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
+</style>
